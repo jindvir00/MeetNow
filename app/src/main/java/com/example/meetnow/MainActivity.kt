06e67a21b59f051952348e8a5f19b36c0c6792c8
JINDVIR SINGH <68278240+jindvir@users.noreply.github.com>
2023-05-18 00:37:51 +0530

    BottomNav commit

--- app/src/main/java/com/example/meetnow/MainActivity.kt
+++ app/src/main/java/com/example/meetnow/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager.widget.ViewPager
 import com.example.meetnow.databinding.ActivityMainBinding
@@ -16,9 +18,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.authenticate.setOnClickListener {
-            startActivity(Intent(this@MainActivity , AuthenticationActivity::class.java))
-            finish()
+//        binding.authenticate.setOnClickListener {
+//            startActivity(Intent(this@MainActivity , AuthenticationActivity::class.java))
+//            finish()
+//        }
+
+        loadFragment(HomeFragment())
+        binding.settingsImg.setOnClickListener {
+            loadFragment(SettingsFragment())
         }
+        binding.itemBottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.notifications -> {
+                    loadFragment(VideoOptionsFragment())
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> {
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.ll_fragment_view ,fragment)
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        transaction.commit()
+
+//        binding.itemBottomNav.selectedItemId = index
     }
 }
